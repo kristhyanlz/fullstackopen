73aff99ba33ffd486c06c6d441e3289f9580dfee
Kurt Zubia <kristhyanlz@gmail.com>
2025-06-08 21:47:38 -0500

    part1 done 1.1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,34 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
 
-function App() {
-  const [count, setCount] = useState(0)
+const Total = ({contents}) => {
+  let total = 0;
+  contents.forEach(element => {
+    total += element.exercises
+  });
 
+  return(
+    <p>
+      Number of exercises {total}
+    </p>
+  )
+}
+
+const Content = ({contents}) => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {
+        contents.map((ele, idx) => (
+          <p key={idx}>
+            {ele.part} {ele.exercises}
+          </p>
+        ))
+      }
     </>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const contents = [
+    { 
+      part: 'Fundamentals of React',
+      exercises : 10
+    },
+    { 
+      part: 'Using props to pass data',
+      exercises: 7
+      
+    },
+    {
+      part: 'State of a component',
+      exercises: 14
+    }
+  ]
+
+  return (
+    <div>
+      <Header course={course}/>
+      <Content contents={contents}/>
+      <Total contents={contents}/>
+    </div>
   )
 }
 
